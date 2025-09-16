0e6f332571e46324a8b964ccfead7a68d4c78d8f
IT24102412 <IT24102412@my.sliit.lk>
2025-09-16 09:49:58 +0530

    Add files via upload

---
+++ Lab Sheet 06/it24102412.R
@@ -0,0 +1,14 @@
+setwd("C:\\Users\\minun\\OneDrive\\Desktop\\it24102412_ps_lab6")
+
+#1
+#i) Random variable X has a Binomial distribution with n=50, p=0.85
+
+dbinom(47,50,0.85)
+pbinom(47,50,0.85,lower.tail = FALSE)
+
+#2
+#i) Average customer calls receives a center (per hour)
+#ii) Random variable X has a poisson distribution with lambda = 12
+
+#iii)
+dpois(15,12)
